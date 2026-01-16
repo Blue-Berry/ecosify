ffe88f18cc83482d92493633d04f767891dabc9f
Liam Berry <liamandberry@gmail.com>
2026-01-16 19:17:27 +0200

    update model

--- lib/model.ml
+++ lib/model.ml
@@ -1,36 +1,14 @@
 open! Core
 
-(*What I want:
-             let grid = decision_variable(size, cost)
-             let gen = decision_variable(size, cost)
-             let pv = decision_variable(size, cost)
-             let bat_chrg = decision_variable(size, cost)
-             let bat_dis = decision_variable(size, cost)
-             let bat_soc = decision_variable(size, cost)
-             let load = const(size, data)
-             add_ineq (gridadd_ineq>= 0);
-             add_ineq (gen >= 0);
-             add_ineq (pv >= 0);
-             add_expr ( grid + gen + pv + bat_dis - bat_chrg = load);
-             for t = 1 to 24 do
-               add_expr ( soc[t+1] = soc[t]+ bat_chrg[t]*dt - bat_dis[t]*dt);
-             done;
-
-ideas infix operators for >=, +, -, = etc
-decision variables are lists
- *)
 module Var = struct
   type vec [@@deriving sexp_of]
   type atom [@@deriving sexp_of]
-  type matrix
   type var_id = int [@@deriving sexp_of]
 
-  (* Operations should work for individual values and vector of values *)
   type _ t =
     | Atom : var_id -> atom t
     | Vec : atom t array -> vec t
   [@@deriving sexp_of]
-  (* | Matrix : atom t array array -> matrix t *)
 
   type _ witness =
     | Atom_wit : atom witness
@@ -42,14 +20,6 @@ module Var = struct
     | Vec _ -> Vec_wit
   ;;
 
-  (* let rec sexp_of_t : type a. a t -> Sexp.t = function *)
-  (*   | Atom id -> Int.sexp_of_t id *)
-  (*   | Vec atoms -> Array.sexp_of_t sexp_of_t atoms *)
-  (* ;; *)
-
-  let ( .%{} ) (Vec ts : vec t) i = ts.(i)
-  let ( .%{}<- ) (Vec ts : vec t) i v = Array.set ts i v
-
   let size : type a. a t -> int = function
     | Atom _ -> 1
     | Vec ts -> Array.length ts
@@ -57,11 +27,12 @@ module Var = struct
 end
 
 type 'a expr =
-  | Const of 'a Var.witness * float
-  | Var of 'a Var.t
-  | Add of 'a expr * 'a expr
-  | Sub of 'a expr * 'a expr
-  | Mul of float * 'a expr
+  | Const : float -> Var.atom expr
+  | Var : 'a Var.t -> 'a expr
+  | Add : 'a expr * 'a expr -> 'a expr
+  | Sub : 'a expr * 'a expr -> 'a expr
+  | Mul : float * 'a expr -> 'a expr
+  | Data : float array -> Var.vec expr
 [@@deriving sexp_of]
 
 type 'a constr =
@@ -76,11 +47,12 @@ type constr_packed =
 [@@deriving sexp_of]
 
 let rec witness_of_expr : type a. a expr -> a Var.witness = function
-  | Const (w, _) -> w
+  | Const _ -> Var.Atom_wit
   | Var e -> Var.witness_of_t e
   | Add (l, _) -> witness_of_expr l
   | Sub (l, _) -> witness_of_expr l
   | Mul (_, r) -> witness_of_expr r
+  | Data _ -> Var.Vec_wit
 ;;
 
 let pack_contr : type a. a Var.witness -> a constr -> constr_packed =
@@ -106,6 +78,18 @@ module Infix = struct
   let ( + ) : type a. a expr -> a expr -> a expr = fun l r -> Add (l, r)
   let ( - ) : type a. a expr -> a expr -> a expr = fun l r -> Sub (l, r)
   let ( * ) : type a. float -> a expr -> a expr = fun l r -> Mul (l, r)
+
+  let ( .%{} ) (expr : vec Var.t expr) i =
+    match expr with
+    | Var (Vec x) -> x.(i)
+    | _ -> failwith "Can't index non variable expressions"
+  ;;
+
+  let ( .%{}<- ) (expr : vec Var.t expr) i v =
+    match expr with
+    | Var (Vec x) -> Array.set x i v
+    | _ -> failwith "Can't index non variable expressions"
+  ;;
 end
 
 type ws =
@@ -127,3 +111,5 @@ let variables ws size =
           incr ws.vars;
           Var.Atom !(ws.vars))))
 ;;
+
+let const_of_list xs = Data (Array.of_list xs)
--- lib/model_test.ml
+++ lib/model_test.ml
@@ -7,7 +7,7 @@ let%expect_test "simepl atom expr" =
   let x1 = Var (Var.Atom 1) in
   let x2 = Var (Var.Atom 2) in
   let x3 = Var (Var.Atom 3) in
-  let l = Const (Var.Atom_wit, 10.) in
+  let l = Const 10. in
   let a = x1 + x2 + x3 == l in
   Model.sexp_of_constr_packed a |> print_s;
   [%expect
@@ -24,7 +24,7 @@ let%expect_test "simepl atom expr 2" =
   let x1 = variable ws in
   let x2 = variable ws in
   let x3 = variable ws in
-  let l = Const (Var.Atom_wit, 10.) in
+  let l = Const 10. in
   let open Infix in
   let a = x1 + x2 + x3 == l in
   Model.sexp_of_constr_packed a |> print_s;
@@ -42,9 +42,9 @@ let%expect_test "simepl vec expr" =
   let x1 = variables ws 10 in
   let x2 = variables ws 10 in
   let x3 = variables ws 10 in
-  let l = Const (Var.Vec_wit, 10.) in
+  let g = const_of_list [ 1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9.; 10. ] in
   let open Infix in
-  let a = x1 + x2 + x3 == l in
+  let a = x1 + x2 + x3 == g in
   Model.sexp_of_constr_packed a |> print_s;
   [%expect
     {|
@@ -64,6 +64,41 @@ let%expect_test "simepl vec expr" =
         (Vec
          ((Atom 21) (Atom 22) (Atom 23) (Atom 24) (Atom 25) (Atom 26) (Atom 27)
           (Atom 28) (Atom 29) (Atom 30)))))
+      (Const Vec_wit 10)))
+    |}]
+;;
+
+let%expect_test "simepl vec expr" =
+  let open Model in
+  let ws = new_ws () in
+  let x1 = variables ws 10 in
+  let x2 = variables ws 10 in
+  let x3 = variables ws 10 in
+  let g = const_of_list [ 1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9.; 10. ] in
+  let l = Const (Var.Vec_wit, 10.) in
+  let open Infix in
+  let a = x1 + x2 + x3 == l in
+  Model.sexp_of_constr_packed a |> print_s;
+  [%expect
+    {|
+    (Constr_vec
+     (Eq
+      (Add
+       (Add
+        (Add
+         (Var
+          (Vec
+           ((Atom 1) (Atom 2) (Atom 3) (Atom 4) (Atom 5) (Atom 6) (Atom 7)
+            (Atom 8) (Atom 9) (Atom 10))))
+         (Var
+          (Vec
+           ((Atom 11) (Atom 12) (Atom 13) (Atom 14) (Atom 15) (Atom 16) (Atom 17)
+            (Atom 18) (Atom 19) (Atom 20)))))
+        (Var
+         (Vec
+          ((Atom 21) (Atom 22) (Atom 23) (Atom 24) (Atom 25) (Atom 26) (Atom 27)
+           (Atom 28) (Atom 29) (Atom 30)))))
+       (Data (1 2 3 4 5 6 7 8 9 10)))
       (Const Vec_wit 10)))
     |}]
 ;;
